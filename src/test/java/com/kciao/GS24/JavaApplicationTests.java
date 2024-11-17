333bf9098202190d3ce74e69eb1a32a10abb5a8d
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2024-11-17 15:35:53 -0300

    First commit

---
+++ src/test/java/com/kciao/GS24/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kciao.GS24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
